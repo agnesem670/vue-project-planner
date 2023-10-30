14d5ef8f6a7c5518addcdcc7e702b8cb21136c6a
agnesem670 <agnesem670@gmail.com>
2023-10-30 13:43:08 +0100

    SingleProject component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,17 +2,17 @@
   <div class="home">
     <div v-if="projects.length">
       <div v-for="project in projects" :key="project.id">
-        <p>{{ project.title }}</p>
+        <SingleProject :project="project"/>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-
+import SingleProject from '../components/SingleProject.vue'
 export default {
   name: 'HomeView',
-  components: {},
+  components: { SingleProject },
   data() {
     return {
       projects: []
@@ -20,9 +20,9 @@ export default {
   },
   mounted() {
     fetch('http://localhost:3000/projects')
-    .then(res => res.json())
-    .then(data => this.projects = data)
-    .catch(err => console.log(err.message))
+      .then(res => res.json())
+      .then(data => this.projects = data)
+      .catch(err => console.log(err.message))
   }
 }
 </script>
